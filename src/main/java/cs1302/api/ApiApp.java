bc887436e372e4684c27096e0d285c5081a7b400
Hannah Le <hl25698@uga.edu>
2024-04-30 17:17:33 -0400

    added getWeather and getPun as well as alert method

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -8,11 +8,34 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+import java.io.IOException;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextArea;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
  */
 public class ApiApp extends Application {
+
+    /** HTTP client. */
+    public static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)
+        .followRedirects(HttpClient.Redirect.NORMAL)
+        .build();
+
+    /** Google {@code Gson} object for parsing JSON-formatted strings. */
+    public static Gson GSON = new GsonBuilder()
+        .setPrettyPrinting()
+        .create();
+
     Stage stage;
     Scene scene;
     VBox root;
@@ -52,5 +75,69 @@ public class ApiApp extends Application {
         stage.show();
 
     } // start
+
+    public void getWeather(String city) {
+        String url = "http://localhost:3000/weather/" + city;
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .build();
+        try {
+            HttpResponse<String> response = HTTP_CLIENT.send(
+                request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException ie) {
+            showAlert("HTTP request failed for URL: " + url, ie);
+        }
+    }
+
+    public void getPun(String keyword) {
+        String url = "https://punapi.rest/api/pun/search?query=" + keyword;
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .build();
+        try {
+            HttpResponse<String> response = HTTP_CLIENT.send(
+                request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException ie) {
+            showAlert("HTTP request failed for URL: " + url, ie);
+        }
+    }
+
+        /**
+     * Shows an alert box with a specific message.
+     *
+     * @param message the message displayed in the alert box
+     */
+    private void showAlert(String message) {
+        TextArea text = new TextArea(message);
+        text.setEditable(false);
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Error");
+        alert.getDialogPane().setContent(text);
+        alert.setResizable(true);
+        alert.showAndWait();
+    }
+
+    /**
+     * Shows an alert box with specific messages regarding the exception.
+     *
+     * @param url the url that caused the exception. Includes the search term and media type.
+     * @param t the throwable that was caught, causing the alert
+     */
+    private void showAlert(String url, Throwable t) {
+        String errorMessage = null;
+        if (t != null) {
+            errorMessage = String.format("URI: %s\n\nException: %s",
+                 url, t.toString());
+        }
+        TextArea text = new TextArea(errorMessage);
+        text.setEditable(false);
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Error");
+        alert.getDialogPane().setContent(text);
+        alert.setResizable(true);
+        alert.showAndWait();
+    }
 
 } // ApiApp
